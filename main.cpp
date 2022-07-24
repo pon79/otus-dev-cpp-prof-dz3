f70f55942f11f75305e3c57a7dcabe00eafb8bea
pon79 <pon79job@gmail.com>
2022-07-24 11:21:55 +0300

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    try
+    {        
+
+
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
