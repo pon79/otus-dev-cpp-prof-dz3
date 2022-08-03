1741bdc5646b03d500c8a677e2e9e8420e15f20f
pon79 <pon79job@gmail.com>
2022-08-03 15:04:13 +0300

    MyAllocate: add usings

--- myallocator.h
+++ myallocator.h
@@ -12,6 +12,11 @@ public:
 
     using value_type = T;
 
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+
     T *allocate( std::size_t n ) {
 
         if( n == 0u )
@@ -36,4 +41,6 @@ public:
     void destroy(U *p) {
         p->~U();
     }
+
+
 };
